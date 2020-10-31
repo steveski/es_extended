536d248d02cc508aded61ebf64aca8f262336d12
Bastiti <37415063+Bastiti@users.noreply.github.com>
2020-10-31 11:21:50 +0100

    Update fr.lua

--- modules/__base__/vehicleshop/data/locales/fr.lua
+++ modules/__base__/vehicleshop/data/locales/fr.lua
@@ -11,5 +11,32 @@
 --   This copyright should appear in every part of the project code
 
 Translations = {
-  ['vehicleshop:press_access'] = 'Appuyer sur ~INPUT_CONTEXT~ pour avoir accès au concessionnaire.'
+  ['blip_buy_title'] = 'concessionnaire (Buy)',
+  ['blip_sell_title'] = 'concessionnaire (Sell)',
+  ['press_to_sell'] = 'Appuyez sur ~INPUT_CONTEXT~ pour vendre ce véhicule.',
+  ['must_be_in_vehicle'] = 'You must be in a vehicle to use this.',
+  ['already_in_vehicle'] = 'Vous êtes déjà dans un véhicule.',
+  ['shop_title'] = 'concessionnaire',
+  ['test_drive'] = 'Test Drive',
+  ['buy_confirm'] = 'Acheter %s pour $%s ?',
+  ['generate_failure'] = 'Erreur avec la génération de la plaque. Veuillez contacter un administateur.',
+  ['buy_success'] = 'Vous avez acheté une ~y~%s~s~ avec la plaque ~b~%s~s~ pour ~g~$%s',
+  ['test_drive_started'] = 'Essai routier démarré.',
+  ['test_drive_remaining_long'] = '~INPUT_CONTEXT~ Pour terminer l\'essai routier plus tôt - Temps restant: ~g~%s',
+  ['test_drive_remaining_med'] = '~INPUT_CONTEXT~ Pour terminer l\'essai routier plus tôt - Temps restant: ~y~%s',
+  ['test_drive_remaining_short'] = '~INPUT_CONTEXT~ Pour terminer l\'essai routier plus tôt - Temps restant: ~r~%s',
+  ['end_test_drive_death'] = 'Essai routier terminé causé par un décès.',
+  ['test_drive_ended'] = 'Essai routier terminé.',
+  ['top_speed'] = 'Vitesse Maximale',
+  ['acceleration'] = 'Accélération',
+  ['gears'] = 'Vitesses',
+  ['seating_capacity'] = 'Nombre de places',
+  ['sell_success'] = 'Vous avez vendu votre ~y~%s~s~ avec la plaque ~b~%s~s~ pour ~g~$%s',
+  ['must_own_vehicle'] = '~r~Vous devez posséder ce véhicule pour utiliser ce marqueur.',
+  ['model_loading'] = 'Veuillez attendre que le modèle se charge...',
+  ['server_buy_success'] = '^7[^4%s^7 |^5 %s %s^7] ^3a acheté^7: ^5%s^7 avec la plaque ^3%s ^7pour ^2$%s^7',
+  ['server_buy_failure'] = '^1Erreur avec l\'achat du véhicule. Veuillez contacter un administateur.^7',
+  ['server_sell_success'] = '^7[^4%s^7 |^5 %s %s^7] ^3a vendu^7: ^5%s^7 avec la plaque ^3%s ^7pour ^2$%s^7',
+  ['server_sell_failure'] = '^1Erreur avec la vente du véhicule. Veuillez contacter un administateur.^7',
+  ['press_access'] = 'Appuyez sur ~INPUT_CONTEXT~ pour accèder au concessionnaire.'
 }
