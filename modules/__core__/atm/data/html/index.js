321454ebb7b31c4e920e34d9d095643525f42f69
itschip <onlygamernerd@gmail.com>
2020-11-26 15:43:26 +0100

    feat(accounts/atm/nui): initial functions for HTTP post requests

--- modules/__core__/atm/data/html/index.js
+++ modules/__core__/atm/data/html/index.js
@@ -3,19 +3,55 @@ window.addEventListener('message', (event) => {
   let msg = event.data
 
   if (msg.method == 'sendCredentials') {
-    document.getElementById('player-name').innerHTML = `Welcome, ${msg.playerName}`
-    document.getElementById('player-balance').innerHTML = `Balance: $${msg.balance}`
+    document.getElementById('player-name').innerHTML = `Welcome, ${msg.data.playerName}`
+    document.getElementById('player-balance').innerHTML = `Balance: $${msg.data.balance}`
   }
+
 })
+
+const depositMoney = () => {
+  console.log("I just deposited money")
+
+  $.post("http://es_extended/esx:atm:close", JSON.stringify({
+    amount: inputAmount
+  }))
+}
+
+const withdrawMoney = () => {
+  console.log("I have taken money away from my balance :(")
+  $.post("http://es_extended/esx:atm:withdraw", JSON.stringify({
+    amount: inputAmount
+  }))
+}
+
+const transferMoney = () => {
+  // should send both the ID and the amount
+  $.post("http://es_extended/esx:atm:transfer", JSON.stringify({
+    playerId: playerId,
+    amount: inputAmount
+  }))
+}
+
+document.onkeyup = (data) => {
+  if (data.which == 27) {
+    // will close the atm when ESC is pressed
+  }
+}
+
+
 
 setTimeout(() => {
   window.dispatchEvent(
     new MessageEvent("message", {
       data: {
         method: 'sendCredentials',
-        playerName: 'John Doe',
-        balance: 400
+        data: {
+          playerName: 'John Doe',
+          balance: 400
+        }
       },
     })
   );
 }, 1000);
+
+
