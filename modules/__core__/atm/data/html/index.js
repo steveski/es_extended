4643875f22831b94b6776f0414389df36b333cad
itschip <onlygamernerd@gmail.com>
2020-11-26 16:35:37 +0100

    feat(accounts/atm/nui): resolved HTTP requests and getting value from input

--- modules/__core__/atm/data/html/index.js
+++ modules/__core__/atm/data/html/index.js
@@ -1,4 +1,4 @@
-
+$('.container').hide();
 window.addEventListener('message', (event) => {
   let msg = event.data
 
@@ -7,12 +7,20 @@ window.addEventListener('message', (event) => {
     document.getElementById('player-balance').innerHTML = `Balance: $${msg.data.balance}`
   }
 
+  if (msg.method == 'setVisibility') {
+    if (msg.data) {
+      $('.container').show();
+    } else if (!msg.data) {
+      $('.container').hide();
+    }
+  }
+
 })
 
 const depositMoney = () => {
   console.log("I just deposited money")
 
-  $.post("http://es_extended/esx:atm:close", JSON.stringify({
+  $.post("http://es_extended/esx:atm:deposit", JSON.stringify({
     amount: inputAmount
   }))
 }
@@ -24,17 +32,34 @@ const withdrawMoney = () => {
   }))
 }
 
+
+let transferAmount;
+
 const transferMoney = () => {
   // should send both the ID and the amount
+  const amount = $('#input-amount').val();
+  $('.transfer-modal').css('visibility', 'visible')
+  transferAmount = amount
+}
+
+const confirmTransfer = () => {
+  const playerId = $('#playerid').val()
   $.post("http://es_extended/esx:atm:transfer", JSON.stringify({
     playerId: playerId,
-    amount: inputAmount
+    amount: transferAmount
   }))
+}
+
+const cancelTransfer = () => {
+  $('.transfer-modal').css('visibility', 'hidden')
 }
 
 document.onkeyup = (data) => {
   if (data.which == 27) {
     // will close the atm when ESC is pressed
+    $('.container').hide();
+    $.post("http://es_extended/esx:atm:close", JSON.stringify({}))
+
   }
 }
 
@@ -54,4 +79,14 @@ setTimeout(() => {
   );
 }, 1000);
 
+setTimeout(() => {
+  window.dispatchEvent(
+    new MessageEvent("message", {
+      data: {
+        method: 'setVisibility',
+        data: true
+      },
+    })
+  );
+}, 1000);
 
