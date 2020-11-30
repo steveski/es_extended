40a6721e7e8c0f18f801a28f9d0f991117074852
Christopher <onlygamernerd@gmail.com>
2020-11-30 22:10:23 +0100

    ✨feat(atm/client): NUI message and registering NUI callbacks

--- modules/__core__/atm/client/module.lua
+++ modules/__core__/atm/client/module.lua
@@ -14,34 +14,46 @@ M('events')
 module.Ready = false
 module.Frame = nil
 
+local atmOpen = false
 
-RegisterNUICallback('esx:atm:close', function()
-  module.Frame:postMessage({
-    method = 'setVisibility',
-    data = false
-  })
-
-  SetNuiFocus(false, false)
+onServer('esx:atm:visibility', function()
+  if not atmOpen then
+    atmOpen = true
+    module.Frame:postMessage({
+      method = 'setVisibility',
+      data = atmOpen
+    })
+    module.Frame:focus()
+    SetNuiFocus(true, true)
+  else
+    atmOpen = false;
+    module.Frame:postMessage({
+      method = 'setVisibility',
+      data = atmOpen
+    })
+    SetNuiFocus(false, false)
+  end
 end)
-
-onServer('esx:atm:display', function()
-  module.Frame:postMessage({
-    method = 'setVisibility',
-    data = true
-  })
-
-  module.Frame:focus()
-  SetNuiFocus(true, true)
-end)
-
 -- Callbacks for each function
 
 RegisterNUICallback('esx:atm:deposit', function(data)
   emitServer('esx:atm:depositMoney', data.amount)
 end)
 
-RegisterNUICallback('esx:atm:deposit', function(data)
+RegisterNUICallback('esx:atm:withdraw', function(data)
   emitServer('esx:atm:withdrawMoney', data.amount)
+  --print('amount: ', data.amount)
 end)
 
+RegisterNUICallback('esx:atm:transfer', function(data)
+    emitServer('esx:atm:transferMoney', data.amount, data.playerId)
+end)
 
+RegisterNUICallback('esx:atm:close', function ()
+  atmOpen = false;
+  module.Frame:postMessage({
+    method = 'setVisibility',
+    data = false
+  })
+  SetNuiFocus(false, false)
+end)
