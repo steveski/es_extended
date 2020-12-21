dd8cf3e1842a3d097648c45bf9143306508d71c1
Ark <arkseyonet@gmail.com>
2020-12-21 00:59:43 -0600

    Update sit module
    - more optimizations

--- modules/__base__/sit/client/main.lua
+++ modules/__base__/sit/client/main.lua
@@ -18,18 +18,22 @@ module.Config = run('data/config.lua', {vector3 = vector3})['Config']
 
 ESX.SetInterval(1, function()
   if not module.Sitting then
-    if IsControlJustPressed(0, module.Config.SitKey) and module.NearObject and IsInputDisabled(0) and IsPedOnFoot(PlayerPedId()) and not module.Sitting then
-      module.Sit(module.Object)
-    end
+    if module.Object then
+      if IsControlJustPressed(0, module.Config.SitKey) and module.Object and IsInputDisabled(0) and IsPedOnFoot(PlayerPedId()) then
+        if module.Object then
+          module.Sit(module.Object)
+        end
+      end
 
-    if IsControlJustPressed(0, module.Config.LieDownKey) and module.NearObject and IsInputDisabled(0) and IsPedOnFoot(PlayerPedId()) and not module.Sitting then
-      if module.Object.type == "bed" then
-        module.LieDown(module.Object)
+      if IsControlJustPressed(0, module.Config.LieDownKey) and module.Object and IsInputDisabled(0) and IsPedOnFoot(PlayerPedId()) then
+        if module.Object then
+          if module.Object.type == "bed" then
+            module.LieDown(module.Object)
+          end
+        end
       end
     end
-  end
-
-  if module.Sitting then
+  else
     if IsControlJustPressed(0, module.Config.GetUpKey) and module.Sitting and IsInputDisabled(0) then
       module.Wakeup()
     end
@@ -37,23 +41,19 @@ ESX.SetInterval(1, function()
     if IsControlJustPressed(0, module.Config.SwitchPositionKey) then
       module.SwitchPositions(module.Object)
     end
-  end
-end)
 
-ESX.SetInterval(200, function()
-  module.Coords = GetEntityCoords(PlayerPedId())
-end)
-
-ESX.SetInterval(1, function()
-  if module.Sitting and module.Coords then
-    if module.Object.type == "chair" then
-      utils.ui.draw3DText(module.Coords.x, module.Coords.y, module.Coords.z-0.5, 0, 0, 0, 0, tostring(_U('sit:press_getup')))
-    elseif module.Object.type == "bed" then
-      utils.ui.draw3DText(module.Coords.x, module.Coords.y, module.Coords.z-1.0, 0, 0, 0, 0, tostring(_U('sit:press_getup_switch')))
+    if module.Coords then
+      if module.Object.type == "chair" then
+        utils.ui.draw3DText(module.Coords.x, module.Coords.y, module.Coords.z-0.5, 0, 0, 0, 0, tostring(_U('sit:press_getup')))
+      elseif module.Object.type == "bed" then
+        utils.ui.draw3DText(module.Coords.x, module.Coords.y, module.Coords.z-1.0, 0, 0, 0, 0, tostring(_U('sit:press_getup_switch')))
+      end
     end
-  else
-    Wait(1000)
   end
+end)
+
+ESX.SetInterval(500, function()
+  module.Coords = GetEntityCoords(PlayerPedId())
 end)
 
 ESX.SetInterval(500, function()
@@ -64,31 +64,41 @@ ESX.SetInterval(500, function()
       local distanceDiff  = #(coordsObject - module.Coords)
 
       if (distanceDiff < module.Config.MaxDistance) then
-        if v.type == "chair" then
-          if not module.DrawActive then
-            module.Draw = {
-              x    = coordsObject.x,
-              y    = coordsObject.y,
-              z    = coordsObject.z,
-              text = tostring(_U('sit:press_chair'))
+        if not module.DrawActive then
+          if v.type == "chair" then
+            module.Object = {
+              name           = closestObject,
+              coords         = coordsObject,
+              type           = v.type,
+              scenario       = v.scenario,
+              scenario2      = v.scenario2,
+              verticalOffset = v.verticalOffset,
+              forwardOffset  = v.forwardOffset,
+              leftOffset     = v.leftOffset,
+              text           = tostring(_U('sit:press_chair'))
+            }
+
+            module.DrawActive = true
+          elseif v.type == "bed" then
+            module.Object = {
+              name           = closestObject,
+              coords         = coordsObject,
+              type           = v.type,
+              scenario       = v.scenario,
+              scenario2      = v.scenario2,
+              verticalOffset = v.verticalOffset,
+              forwardOffset  = v.forwardOffset,
+              leftOffset     = v.leftOffset,
+              text           = tostring(_U('sit:press_bed'))
             }
 
             module.DrawActive = true
           end
-        elseif v.type == "bed" then
-          if not module.DrawActive then
-            module.Draw = {
-              x    = coordsObject.x,
-              y    = coordsObject.y,
-              z    = coordsObject.z,
-              text = tostring(_U('sit:press_bed'))
-            }
 
-            module.DrawActive = true
-          end
+          break
+        else
+          Wait(1000)
         end
-
-        break
       else
         module.DrawActive = false
       end
@@ -99,10 +109,13 @@ ESX.SetInterval(500, function()
 end)
 
 ESX.SetInterval(1, function()
-  if module.DrawActive then
-    if module.Draw then
-      if module.Draw.x and module.Draw.y and module.Draw.z and module.Draw.text then
-        utils.ui.draw3DText(module.Draw.x, module.Draw.y, module.Draw.z, 0, 0, 0, 0, module.Draw.text)
+  if module.Sitting then
+    module.DrawActive = false
+    Wait(1000)
+  else
+    if module.DrawActive then
+      if module.Object then
+        utils.ui.draw3DText(module.Object.coords.x, module.Object.coords.y, module.Object.coords.z, 0, 0, 0, 0, module.Object.text)
       end
     end
   end
